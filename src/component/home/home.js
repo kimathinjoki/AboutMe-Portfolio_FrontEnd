396cec651dbacc8427e510b592e6462815dec249
kimathinjoki <dfkimathinjoki@gmail.com>
2023-02-28 05:07:31 -0800

    first commit

---
+++ src/component/home/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../navbar/navbar";
+
+function Home(){
+    return(
+        <>
+
+        <Navbar/>
+
+
+        </>
+    )
+}
+
+export default Home;
---
+++ src/component/navbar/navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar(){
+    return(
+
+        
+        <nav id="navbar" >
+            <a href="#" class="logo">ME<span>+</span></a>
+            <a href="#">HOME</a>
+            <a href="#">PORTFOLIO</a>
+            <a href="#">ABOUT</a>
+            <a href="#">ADD+</a>
+            <div id="indicator"></div>     
+        </nav>
+    )
+}
+
+export default Navbar;
