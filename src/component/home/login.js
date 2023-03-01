9cb22718e0e15cd0ff3412788fe5c34d5f6db54d
kimathinjoki <dfkimathinjoki@gmail.com>
2023-03-01 11:17:11 -0800

    add bio form

--- src/component/home/login.js
+++ src/component/home/login.js
@@ -3,17 +3,21 @@ import React from "react";
 function Login(){
 
 
-    const signUpButton = document.getElementById('signUp');
-    const signInButton = document.getElementById('signIn');
-    const container = document.getElementById('container');
 
-    signUpButton.addEventListener('click', () => {
-	    container.classList.add("right-panel-active");
-    });
 
-    signInButton.addEventListener('click', () => {
-	    container.classList.remove("right-panel-active");
-    });
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+ container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+container.classList.remove("right-panel-active");
+});
+
+    
 
     return(
         <div>
