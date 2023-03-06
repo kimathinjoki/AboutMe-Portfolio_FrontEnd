2c9ff4ea37484e518cf6e7e1a634748731bccb65
kimathinjoki <dfkimathinjoki@gmail.com>
2023-03-05 16:15:40 -0800

    adaded a form

--- src/component/portfolio/PortfolioItem.js
+++ src/component/portfolio/PortfolioItem.js
@@ -2,7 +2,26 @@ import React from 'react';
 import avi from '../../avi.png'
 import './portfolioitem.css'
 
-function PortfolioItem() {
+function PortfolioItem({name, email, skills, id}) {
+	
+	const [skillName, setSkillName] = React.useState('');
+
+	function addSkill(){
+		fetch('http://127.0.0.1:9292/skills/create',{
+			method: 'POST',
+            headers: {
+				'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			"name": skillName,
+			"user_id": id
+		}),
+	})
+	.then(res => res.json())
+	
+	}
+
+
 	return (
 		<div className="portfolio-item">
 			<div className="avatar">
@@ -11,15 +30,23 @@ function PortfolioItem() {
 				</div>
 			</div>
 			<div className="name">
-				<p>KIMATHI</p>
-				<p>kim@multi-work.com</p>
+				<p>{name}</p>
+				<p>{email}</p>
 				<h4>Skills</h4>
 				<ul>
-					<li>Ruby</li>
-					<li>Python</li>
+					{skills.map( (skill)=> <li key={skill.id}>{skill.name}</li>)}
+					{/* <li>Python</li>
 					<li>JavaScript</li>
-					<li>Bash</li>
+					<li>Bash</li> */}
 				</ul>
+				<div>
+					<input type="text" placeholder='Add skill' value={skillName} onChange={(e)=>{
+						e.preventDefault()
+						setSkillName(e.target.value)
+					}}/>
+					<button onClick={addSkill()}>+</button>
+				</div>
+				
 			</div>
 			<div className="skills">
 				{/* <p>Ruby, Python, JavaScript, Bash</p> */}
