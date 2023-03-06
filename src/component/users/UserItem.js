7577a6a16b5f5df462b5c74f05aa646327d9e0ff
kimathinjoki <dfkimathinjoki@gmail.com>
2023-03-05 16:14:48 -0800

    added content

--- src/component/users/UserItem.js
+++ src/component/users/UserItem.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 
-function UserItem(avatar_url){
+function UserItem(avatar_url, name, email, id){
     return(
         <div>
-            <img src={avatar_url} alt="Avatar" />
+            
+            <div key={id} className="projects-card">
+                <div className="img">
+					<img src={avatar_url} alt="avatar" title='user-avatar' />
+				</div>
+                <p className="project-title">{name}</p>
+                <p>{email}</p>
+                <button>VIEW</button>
+             
+            </div> 
 
         </div>
     )
